b1a21da8bf65222f42a8c16e2a7758f62ea297d0
Ashraf Elshowen <ashrafelshowen@gmail.com>
2022-02-20 20:23:21 +0200

    Add project files.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace Ashraf_Letters_Speaker_App
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(135, 82);
+            this.textBox1.MaxLength = 1;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 38);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(30, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(330, 31);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Enter the latter in the box :\r\n";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(379, 141);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "Ashraf Letter Speaker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ MainForm.cs
@@ -0,0 +1,42 @@
+﻿using System.Speech.Synthesis;
+using System.Windows.Forms;
+
+namespace Ashraf_Letters_Speaker_App
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e) => textBox1.Text = Validate_Speak_Letter(e);
+
+        private string SpeakLetter(string _text)
+        {
+            // upper case : 65 to 90
+            //lower case : 97 to 122
+            //space 32
+            SpeechSynthesizer speechSynthesizer = new SpeechSynthesizer();
+            speechSynthesizer.SetOutputToDefaultAudioDevice();
+            speechSynthesizer.Speak(_text);
+            return _text;
+        }
+
+        private string Validate_Speak_Letter(KeyEventArgs e)
+        {
+            int _keyCode = (int)e.KeyCode;
+            if ((_keyCode >= 65 && _keyCode <= 90) ||
+                    (_keyCode >= 97 && _keyCode <= 122))
+            {
+                string letter = e.KeyCode.ToString();
+                SpeakLetter(letter);
+                return letter;
+            }
+            else
+            {
+                return string.Empty;
+            }
+        }
+    }
+}
